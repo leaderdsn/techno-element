43c6a26cd060e811e19f8a8eea2019e8c01fecf1
leaderdsn <leaderdsn.it@gmail.com>
2021-02-25 19:15:32 +0800

    fixed the submit functionality when sending to the server

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,4 +1,3 @@
-let editingCardEl = null;
 let cardData = {
     id: null,
     value: null
@@ -9,7 +8,7 @@ export const createCard = (card) => {
 
     // container for cards
     const droppableElem = document.createElement('div');
-    droppableElem.id = 'drop-' + card.id;
+    droppableElem.id = `drop-${card.id}`;
     droppableElem.classList.add(`droppable`);
 
     // product card
@@ -23,10 +22,14 @@ export const createCard = (card) => {
     // card name
     const cardTitleElem = document.createElement('h2');
     cardTitleElem.classList.add('title');
+    cardTitleElem.innerHTML = card.title;
+    cardElem.appendChild(cardTitleElem);
 
     // card description
     const cardDescriptionElem = document.createElement('div');
     cardDescriptionElem.classList.add('description');
+    cardDescriptionElem.innerHTML = card.description;
+    cardElem.appendChild(cardDescriptionElem);
 
     // container for price and button elements
     const cardFooterElem = document.createElement('div');
@@ -35,82 +38,72 @@ export const createCard = (card) => {
     // card price
     const priceElem = document.createElement('div');
     priceElem.classList.add('price');
+    priceElem.innerHTML = card.price;
+    cardFooterElem.appendChild(priceElem);
 
     // buy button
     const btnBuyElem = document.createElement('div');
-    btnBuyElem.classList.add('btn');
-    btnBuyElem.classList.add('btn-hs-sm');
-    btnBuyElem.classList.add('btn-ws-md');
-    btnBuyElem.classList.add('btn-warning');
-
-    cardTitleElem.innerHTML = card.title;
-    cardDescriptionElem.innerHTML = card.description;
-    priceElem.innerHTML = card.price;
+    btnBuyElem.classList.add('btn', 'btn-hs-sm', 'btn-ws-md', 'btn-warning', 'br');
     btnBuyElem.innerHTML = 'Buy';
+    cardFooterElem.appendChild(btnBuyElem);
 
-    cardFooterElem.appendChild(priceElem)
-    cardFooterElem.appendChild(btnBuyElem)
-
-    cardElem.appendChild(cardTitleElem);
-    cardElem.appendChild(cardDescriptionElem);
     cardElem.appendChild(cardFooterElem);
     droppableElem.appendChild(cardElem);
     cardContainerElem.appendChild(droppableElem);
 
     cardTitleElem.addEventListener('click', (e) => {
-        showModalForm(e, cardTitleElem)
+        showModalForm(e, cardTitleElem);
     })
 };
 
 const listenFormCardSubmit = (elem) => {
-    const form = document.querySelector('.js-form-card')
+    const form = document.querySelector('.js-form-card');
 
-    form.onsubmit = (e) => {
+    form.onsubmit = async(e) => {
         e.preventDefault();
+
         cardData = {
             id: elem.parentNode.id,
             value: e.target.inputForm.value
         }
-        submitCard()
-            .then((res) => {
-                console.log(res)
-                editingCardEl = cardData;
-                elem.innerText = e.target.inputForm.value
-                form.reset();
-            })
-            .catch((err) => {
-                // staging
-                console.error(err)
-            })
-            .finally(() => {
-                editingCardEl = null;
-                cardData = null;
-            })
+
+        try {
+            await submitCard(cardData);
+            form.reset();
+        } catch (e) {
+            form.reset();
+        }
+        cardData = null;
     }
     closeModalForm();
 }
 
 function submitCard(data) {
-    return window.fetch({
-            url: '/postData',
+    return window.fetch('/postData', {
+            method: 'POST',
             body: JSON.stringify(data)
         })
-        .then(() => console.log('success send data'))
-        .catch(() => console.error('failed send data'));
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error('Something went wrong');
+            };
+        });
 };
 
 function showModalForm(e, elem) {
     const modalFormContainer = document.querySelector('.js-modal-form-container');
-    const inputForm = document.querySelector('.js-input-form')
+    const inputForm = document.querySelector('.js-input-form');
     inputForm.value = e.target.innerText;
-    listenFormCardSubmit(elem)
-    modalFormContainer.classList.add('show-modal')
+    listenFormCardSubmit(elem);
+    modalFormContainer.classList.remove('hidden');
 };
 
 function closeModalForm() {
     const modalFormContainer = document.querySelector('.js-modal-form-container');
     const btnClose = document.querySelector('.js-btn-close');
     btnClose.onclick = () => {
-        modalFormContainer.classList.remove('show-modal')
+        modalFormContainer.classList.add('hidden');
     };
 };
